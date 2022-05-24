79598f62ff8dbf976a33404918e9f98c1c88dbe4
BrennoMaturino1 <67515570+BrennoMaturino1@users.noreply.github.com>
2022-05-24 17:27:56 -0300

    Update to BrennCode.Math API
    Now Math is public and can make any operation with the same character amount (like 10+79) but still doesnt accept points and different characters (i think i can add the last one in the next update)

---
+++ BrennCode/BrennCode/Math.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace BrennCode
+{
+    public class Math
+    {
+        /// <summary>
+        /// Add 2 numbers in the form of string, check the method "HasIllegalChars" for n1 and n2 rules
+        /// </summary>
+        /// <param name="n1">First string to add</param>
+        /// <param name="n2">Second string to add</param>
+        /// <returns>string</returns>
+        public static string Add(string n1, string n2)
+        {
+            if (HasIllegalChars(n1) || HasIllegalChars(n2))
+            {
+                return "0";
+            }
+            string[] n1Array = n1.Split();
+            string[] n2Array = n2.Split();
+            if (n1Array.Length == n2Array.Length)
+            {
+                string[] resultArray = { };
+                for (int i = 0; i < n1Array.Length; i++)
+                {
+                    Int64 n1Parsed = Int64.Parse(n1Array[i]);
+                    Int64 n2Parsed = Int64.Parse(n2Array[i]);
+                    Int64 result = n1Parsed + n2Parsed;
+                    string[] resultStr = { result.ToString() };
+                    resultArray = resultArray.Concat(resultStr).ToArray();
+                }
+                return String.Join("", resultArray);
+            }
+            return "bruh";
+        }
+
+        /// <summary>
+        /// Checks for illegal characters in a number, you can use it, but in the operation methods the input is automatically checked
+        /// <para>It should not have any letters or special characters, it only accepts "0-9" and "."</para>
+        /// <para>Also don't start with things like ".5", always do 0.5 or it will be considered illegal</para>
+        /// </summary>
+        /// <param name="n">Number to check</param>
+        /// <returns></returns>
+        public static bool HasIllegalChars(string n)
+        {
+            return !Regex.IsMatch(n, "[0-9.]", RegexOptions.IgnoreCase) && n.StartsWith(".");
+        }
+    }
+}
---
+++ BrennCode/TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrennCode;
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Info.About();
+            ColorfulConsole.WriteLine("hello world", 50, 255, 255);
+            ColorfulConsole.Write("hello ", 255, 0, 0);
+            ColorfulConsole.Write("wor", 0, 255, 0);
+            ColorfulConsole.WriteLine("ld", 0, 0, 255);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(BrennCode.Math.Add("1.0", "1.5"));
+            Console.ReadKey();
+        }
+    }
+}

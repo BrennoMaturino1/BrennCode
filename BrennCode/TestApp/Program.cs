772a57fcf1b3f390403a8f72adc424c2b9b658d2
Brenno <67515570+BrennoMaturino1@users.noreply.github.com>
2022-05-26 20:47:14 -0300

    Add files via upload
    Renamed math class to BMath to avoid conflicts, and now the add method support decimal numbers (next update negative numbers)

--- BrennCode/TestApp/Program.cs
+++ BrennCode/TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,14 @@ namespace TestApp
         static void Main(string[] args)
         {
             Info.About();
+            Info.ChangeLog();
             ColorfulConsole.WriteLine("hello world", 50, 255, 255);
             ColorfulConsole.Write("hello ", 255, 0, 0);
             ColorfulConsole.Write("wor", 0, 255, 0);
             ColorfulConsole.WriteLine("ld", 0, 0, 255);
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(BrennCode.Math.Add("1.0", "1.5"));
+            Console.WriteLine(BMath.Add("1.0", "1.5"));
+
             Console.ReadKey();
         }
     }
